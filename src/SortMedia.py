d7e0afcd5d3969690c7f0cd57f9fee8d098d135f
Volokzhanin Vadim <volokzhanin@yandex.ru>
2021-07-20 16:37:15 +1000

    получение файлов из dwh, запись и получение модели

--- src/SortMedia.py
+++ src/SortMedia.py
@@ -20,6 +20,17 @@ import re
 import datetime
 import pytz
 
+# Для Deep Learning
+import torch
+from torch.utils.data import DataLoader
+from torchvision import datasets
+
+# Для работы с распознаванием лиц
+from facenet_pytorch import MTCNN, extract_face, InceptionResnetV1
+
+# Для сериализации и десериализации объектов Python
+import pickle
+
 ###############################################################################################################################################
 ############################################## Создаем объект класса ##########################################################################
 ###############################################################################################################################################
@@ -30,10 +41,12 @@ class SortMedia():
         ,pg_password
         ,pg_login
         ,pg_host
+        ,device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
         ,type_file_tuple = ('.jpg', '.png', '.mp4')
         ,path_list = ['/camera_vvy', '/camera_angel']
         ,schema = 'sort_media'
         ,table_name = 'path'
+        ,MTCNN = MTCNN
     ):
         """
         Сортировка медиафайлов с найденным лицом искомого человека по папкам. 
@@ -41,18 +54,32 @@ class SortMedia():
             pg_password(str) - пароль к DWH.
             pg_login(str) - логин к DWH. 
             pg_host(str) - хост DWH.
+            device(torch.device) - устройство для работы модели.
             type_file_tuple(tuple) - кортеж с типами медиафайлов.
             path_list(list) -  список путей к файлам.
             schema(str) - наименование схемы DWH.
             table_name - наименование таблицы DWH. 
             type_file(list) - список типов файлов.
             path_list(list) - список путей к файлам. 
+            MTCNN(Model.Model) - модель для распознавания лиц.
+            
         """
         self.type_file_tuple = type_file_tuple
         self.path_list = path_list
         self.engine = create_engine(f'postgres://{pg_login}:{pg_password}@{pg_host}:5432/{pg_login}')
         self.schema = schema
         self.table_name = table_name
+        self.device = device
+        self.mtcnn = MTCNN(
+                    image_size = 160
+                    ,margin = 0
+                    ,min_face_size = 20
+                    ,thresholds = [0.6, 0.7, 0.7]
+                    ,factor = 0.709
+                    ,post_process = True
+                    ,device = device
+                    ,keep_all = True
+                )
         
     def pg_descriptions(self): 
         """
@@ -194,3 +221,80 @@ class SortMedia():
 
         with self.engine.connect() as conn:
             result = conn.execute(update_stmt)
+            
+    def path(self): 
+        """
+        Новые файлы на обработку.
+        Вход: 
+            нет.
+        Выход: 
+            result_df(DataFrame) - таблица с путями к новым файлам.
+        """
+        
+        query = """
+        select
+                "path" 
+                ,type
+        from 
+                sort_media."path"	
+        where 
+        -- Отбираем файлы, котолрые не обработаны
+                is_processed  = False
+        """
+        
+        result_df = pd.read_sql(
+            sql = query
+            ,con = self.engine        
+        )
+        return result_df
+    
+    def write_model(self, path = '/ml_models/sort_media/binary_model.bin'):             
+        """
+        Сохранение модели для распознавания лиц в бинарный файл. 
+        Вход:
+            path(str) - путь для сохранения файла.
+        Выход: 
+            нет. 
+        """        
+
+        resnet = InceptionResnetV1(pretrained = 'vggface2')
+        with open(path, "wb") as f:
+            pickle.dump(resnet, f)            
+            
+    def load_model_embedding(self, path = '/ml_models/sort_media/binary_model.bin'): 
+        """
+        Загрузка модели из бинарного файла для описания ключевых точек.
+        Вход: 
+            path(str) - путь файлу модели.
+        Выход: 
+            model(Model.Model) - модель в режиме оценки, и перенесенная на видеокарту. 
+        """
+        with open(f"{path}", "rb") as f:
+            model = pickle.load(f)
+        return model.eval().to(self.device)
+    
+    def load_model_face_detect(self): 
+        """
+        Загрузка модели для поиска лица.
+        Вход: 
+            нет.
+        Выход: 
+            mtcnn(Model.Model) - модель, перенесенная на видеокарту. 
+        """
+# Создаем объект для распознавания лиц
+        mtcnn = MTCNN(
+            image_size = 160
+            ,margin = 0
+            ,min_face_size = 20
+            ,thresholds = [0.6, 0.7, 0.7]
+            ,factor = 0.709
+            ,post_process = True
+            ,device = self.device
+            ,keep_all = True
+        )
+        return mtcnn
+    
+    
+    
+
+        
